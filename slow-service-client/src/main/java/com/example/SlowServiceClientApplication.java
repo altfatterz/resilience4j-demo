0c69bb97aedd4d243a17224472a3c1e91aee33ab
Zoltan Altfatter <altfatterz@gmail.com>
2019-06-07 18:07:41 +0200

    add timer

--- slow-service-client/src/main/java/com/example/SlowServiceClientApplication.java
+++ slow-service-client/src/main/java/com/example/SlowServiceClientApplication.java
@@ -1,10 +1,11 @@
 package com.example;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.circuitbreaker.commons.CircuitBreakerFactory;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,19 +33,22 @@ class ClientController {
 
     private final RestTemplate restTemplate;
     private final CircuitBreakerFactory circuitBreakerFactory;
+    private final Timer slowServiceTimer;
 
     public ClientController(RestTemplate restTemplate,
-                            CircuitBreakerFactory circuitBreakerFactory) {
+                            CircuitBreakerFactory circuitBreakerFactory,
+                            MeterRegistry meterRegistry) {
         this.restTemplate = restTemplate;
         this.circuitBreakerFactory = circuitBreakerFactory;
+        this.slowServiceTimer = meterRegistry.timer("slow.service");
     }
 
     @GetMapping("/")
     public String hello() {
-        return circuitBreakerFactory.create("slow").run(
-                () -> restTemplate.getForObject("http://slow-service/slow", String.class),
-                throwable -> "fallback");
-
+        return slowServiceTimer.record(() ->
+                circuitBreakerFactory.create("slow.service").run(
+                        () -> restTemplate.getForObject("http://slow-service/slow", String.class),
+                        throwable -> "fallback"));
     }
 
 }
